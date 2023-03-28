4bc41e7af61d15f2b3fb2a6bd8577c826a525221
Konstantin <konstantin.v737@gmail.com>
2023-03-29 00:37:16 +0300

    partial adaptation of the tab

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(document).ready(function(){
         slidesToShow: 2,
         slidesToScroll: 1,
         adaptiveHeight: true,
-        // autoplay: true,
+        autoplay: true,
         autoplaySpeed: 2000,
         pauseOnHover: true,
         pauseOnDotsHover: true,
@@ -18,4 +18,51 @@ $(document).ready(function(){
             }
         ]
     });
-  }); 
+    const tabs = document.querySelectorAll('.tab__item');
+    const tabsContent = document.querySelectorAll('.content-tab');
+
+    tabs.forEach((tab, index) => {
+        tab.addEventListener('click', () => {
+            tabs.forEach(tab => {tab.classList.remove('active')});
+            tab.classList.add('active');
+
+            tabsContent.forEach(content => {content.classList.remove('active')});
+            tabsContent[index].classList.add('active');
+        })
+    });
+
+    // const multiSelect = () => {
+    //     const element = document.querySelectorAll('.tab-select');
+    //     const choices = new Choices(element, {
+    //         searchEnabled: false,
+    //     })
+    // }
+
+    // multiSelect();
+
+    // const multiSelect = () => {
+    //     const elements = document.querySelectorAll('.tab-select');
+    //     elements.forEach(el => {
+    //         const choices = new Choices(el, {
+    //             searchEnabled: false,
+    //             itemSelectText: "",
+    //         });
+    //     });
+    // }
+    
+    // multiSelect();
+
+    const multiSelect = () => {
+        const elements = document.querySelectorAll('.item-box__select');
+        elements.forEach(el => {
+            const choices = new Choices(el, {
+                searchEnabled: false,
+                itemSelectText: "",
+            });
+        });
+    }
+    
+    multiSelect();
+
+
+});
