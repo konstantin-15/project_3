45167a32ef87d1b3c9cbf45ae14a0416a03edcad
Konstantin <konstantin.v737@gmail.com>
2023-03-31 00:17:46 +0300

    adaptation of the tab block

--- js/script.js
+++ js/script.js
@@ -18,8 +18,21 @@ $(document).ready(function(){
             }
         ]
     });
+    // const tabs = document.querySelectorAll('.tab__item');
+    // const tabsContent = document.querySelectorAll('.content-tab');
+
+    // tabs.forEach((tab, index) => {
+    //     tab.addEventListener('click', () => {
+    //         tabs.forEach(tab => {tab.classList.remove('active')});
+    //         tab.classList.add('active');
+
+    //         tabsContent.forEach(content => {content.classList.remove('active')});
+    //         tabsContent[index].classList.add('active');
+    //     })
+    // });
+
     const tabs = document.querySelectorAll('.tab__item');
-    const tabsContent = document.querySelectorAll('.content-tab');
+    const tabsContent = document.querySelectorAll('.flex-box');
 
     tabs.forEach((tab, index) => {
         tab.addEventListener('click', () => {
